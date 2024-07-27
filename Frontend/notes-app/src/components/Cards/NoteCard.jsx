4ae539ffcb5c2bc9f5d92bd7ad17508ad5e647ea
Erick Pinedo <erickfprogrammer@gmail.com>
2024-07-27 14:13:54 -0500

    adding files

---
+++ Frontend/notes-app/src/components/Cards/NoteCard.jsx
@@ -0,0 +1,56 @@
+import moment from "moment";
+import React from "react";
+import { GoArchive } from "react-icons/go";
+import { MdCreate, MdDelete } from "react-icons/md";
+
+const NoteCard = ({
+  title,
+  date,
+  content,
+  tags,
+  isArchived,
+  onEdit,
+  onDelete,
+  onArchiveNote,
+}) => {
+  return (
+    <div className="border rounded p-4 bg-white hover:shadow-xl transition-all ease-in-out">
+      <div className="flex items-center justify-between">
+        <div>
+          <h6 className="text-sm font-medium">{title}</h6>
+          <span className="text-xs text-slate-500">
+            {date ? moment(date).format("Do MMM YYYY") : "-"}
+          </span>
+        </div>
+
+        <GoArchive
+          className={`icon-btn ${
+            isArchived ? "text-primary" : "text-slate-300"
+          }`}
+          onClick={onArchiveNote}
+        />
+      </div>
+
+      <p className="text-xs text-slate-600 mt-2">{content?.slice(0, 60)}</p>
+
+      <div className="flex items-center justify-between mt-2">
+        <div className="text-xs text-slate-500">
+          {tags.map((item) => `#${item} `)}
+        </div>
+
+        <div className="flex items-center gap-2">
+          <MdCreate
+            className="icon-btn hover:text-green-600"
+            onClick={onEdit}
+          />
+          <MdDelete
+            className="icon-btn hover:text-red-500"
+            onClick={onDelete}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoteCard;
